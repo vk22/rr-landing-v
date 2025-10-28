ffaf259ed7bce72dfbff70a1a5793de1ec14edfb
User <v.kushnir22@gmail.com>
2025-10-28 17:47:46 +0200

    stage 2.1

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,17 +4,21 @@
   >
     <section
       id="top"
-      class="mx-auto max-w-5xl flex flex-col items-center text-center py-20 px-10 transition-all duration-2000"
+      class="mx-auto max-w-5xl flex flex-col items-center text-center py-20 px-10"
+    >
+      <div class="logo mb-10 md:mb-16 transition-all duration-1000 delay-100"
       :class="{
         'opacity-100 translate-y-0': contentVisible,
-        'opacity-90 translate-y-0': !contentVisible
-      }"
-    >
-      <div class="logo mb-10 md:mb-16">
-        <img src="../assets/img/logo.svg" alt="" />
+        'opacity-0 translate-y-1': !contentVisible
+      }">
+        <img src="../assets/img/logo.svg" class="w-[200px] md:w-[250px]" alt="" />
       </div>
 
-      <div class="text-1">
+      <div class="text-1 transition-all duration-1000 delay-500"
+      :class="{
+        'opacity-100 translate-y-0': contentVisible,
+        'opacity-0 translate-y-1': !contentVisible
+      }">
         <h1 class="text-white mb-6 md:mb-6 md:mb-16 text-[2rem] md:text-[4rem] max-w-[900px]">
           Riviera Residences — <span class="text-accent color1">a new icon 
        
@@ -24,19 +28,31 @@
         </h1>
       </div>
 
-      <div class="text-2">
+      <div class="text-2 transition-all duration-1000 delay-500"
+       :class="{
+        'opacity-100 translate-y-0': contentVisible,
+        'opacity-0 translate-y-1': !contentVisible
+      }">
         <p class="text-white mb-4 md:mb-8 text-[.9rem] md:text-[1.15rem] max-w-lg">
           Architecture by Herzog & de Meuron — Pritzker Prize laureates and authors of some of the most iconic architecture of the 21st century.
         </p>
       </div>
 
-      <div class="text-2">
+      <div class="text-2 transition-all duration-1000 delay-500"
+       :class="{
+        'opacity-100 translate-y-0': contentVisible,
+        'opacity-0 translate-y-1': !contentVisible
+      }">
         <p class="text-white mb-6 md:mb-12 text-[.9rem] md:text-[1.15rem] max-w-lg">
           Blending Mediterranean serenity with the spirit of the Emirates, Riviera Residences offers the comfort of a private resort — with refined design, waterfront promenade, and world-class amenities.
         </p>
       </div>
 
-      <div class="text-2">
+      <div class="text-2 transition-all duration-1000 delay-500"
+       :class="{
+        'opacity-100 translate-y-0': contentVisible,
+        'opacity-0 translate-y-1': !contentVisible
+      }">
         <p class="text-white text-lg md:text-xl mb-10 md:mb-12 max-w-lg">
           <span class="font-sans text-[1.5rem] md:text-[2rem]">Sales start in</span> <span class="font-sans-italic text-[1.5rem] md:text-[2rem]">November</span>
           <br>
@@ -48,7 +64,11 @@
         <p class="text-white text-lg md:text-xl mb-6 md:mb-8 max-w-lg">Register your interest today </p>
       </div> -->
       
-      <MailchimpForm></MailchimpForm>
+      <MailchimpForm class="transition-all duration-1000 delay-700"
+       :class="{
+        'opacity-100 translate-y-0': contentVisible,
+        'opacity-0 translate-y-1': !contentVisible
+      }"></MailchimpForm>
     
       <div class="text-2">
         <p class="text-white text-m max-w-xl mb-4 md:mb-8">Attendance is by invitation only — spaces are limited, and QR-coded invites will be sent one week before the event. </p>
